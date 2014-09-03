0832227d7b6478f93b1c3106ef6a73302b78b630
Amos Ng <amosjyng@gmail.com>
2014-09-03 08:27:31 -0400

    Added io interface

--- src/ProcessManager.java
+++ src/ProcessManager.java
@@ -1,18 +1,24 @@
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.io.ObjectInputStream;
 import java.io.FileOutputStream;
 import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class ProcessManager {
 	
-	private MigratableProcess ins;
+	private static List<MigratableProcess> processes = new ArrayList<MigratableProcess>();
 	
 	Thread t;
 	
 	ObjectOutputStream oos ;
 	
 	FileOutputStream fos;
+	
+	private static BufferedReader stdin = new java.io.BufferedReader(new java.io.InputStreamReader(System.in));
 	
 	private static String pm = "ProcessManager";
 	
@@ -24,30 +30,14 @@ public class ProcessManager {
 		System.err.println("[" + processName + "] " + message);
 	}
 	
-	
-	public  ProcessManager (String processName, String []s) throws Exception{
-		log(pm, "Starting new thread for \"" + processName + "\"");
-		
-		// looked up http://www.rgagnon.com/javadetails/java-0351.html
-		ins = (MigratableProcess)Class.forName(processName).getConstructor(String[].class).newInstance((Object) s);
-		fos = new FileOutputStream("temp.out");
-		oos = new ObjectOutputStream(fos);
-		
-	}
-	
-	
-	public void launch(){
-		
-		t=new Thread(ins);
-		t.start();
-		
-	}
-	public void migrate() throws InterruptedException, IOException, ClassNotFoundException{
+	public void migrate(MigratableProcess ins) throws InterruptedException, IOException, ClassNotFoundException{
 		log(pm, "Migrating thread for \"" + ins.toString() + "\"");
 		
 		log(pm, "Telling \"" + ins.toString() + "\" to suspend.");
 		ins.suspend();
 		log(pm, "Writing out \"" + ins.toString() + "\"");
+		fos = new FileOutputStream("temp.out");
+		oos = new ObjectOutputStream(fos);
 		oos.writeObject(ins);
 		
 		
@@ -60,22 +50,35 @@ public class ProcessManager {
 		log(pm, "Reading in \"" + ins.toString() + "\"");
 		t=new Thread(ins);
 		t.start();
+	}
+	
+	private static MigratableProcess spawnNewProcess(String processName, String[] args) throws Exception {
+		// looked up http://www.rgagnon.com/javadetails/java-0351.html
+		MigratableProcess ins = (MigratableProcess)Class.forName(processName).getConstructor(String[].class).newInstance((Object) args);
 		
-		
-}
+		new Thread(ins).start();
+		return ins;
+	}
+	
+	private static String prompt() throws Exception {
+		System.out.print("> ");
+		return stdin.readLine();
+	}
 	
 	public static void main(String[] args) throws Exception {
-		String []s=new String[3];
-		s[0]="abcde";
-		s[1]="1.txt";
-		s[2]="2.txt";
+		String line = prompt();
+		while (!line.equals("exit")) {
+			String[] stdinArgs = line.split(" ");
+			String processName = stdinArgs[0];
+			
+			log(pm, "Starting new thread for \"" + processName + "\"");
+			processes.add(spawnNewProcess(processName, Arrays.copyOf(Arrays.asList(stdinArgs).subList(1, stdinArgs.length).toArray(), stdinArgs.length - 1, String[].class)));
+			
+			line = prompt();
+		}
 		
-		ProcessManager a=new ProcessManager("GrepProcess", s);
-		a.launch();
 		Thread.sleep(1000);
-		a.migrate();
-		
-		
+		//a.migrate();
 	}
 
 
