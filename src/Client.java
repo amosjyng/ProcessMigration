99269716fea9f162b5319afb390e0d595b82119a
Amos Ng <amosjyng@gmail.com>
2014-09-09 14:11:07 -0400

    Keep listening for processes

--- src/Client.java
+++ src/Client.java
@@ -4,11 +4,11 @@ import java.net.Socket;
 
 public class Client {
   static Socket serverSocket;
-  
+
   private static boolean debug = true;
-  
+
   private static int port;
-  
+
   public static void log(String message) {
     if (debug) {
       System.out.println("[Client:" + port + "] " + message);
@@ -18,15 +18,17 @@ public class Client {
   public static void main(String[] args) throws Exception {
     port = Integer.parseInt(args[1]);
     serverSocket = new Socket(args[0], port);
-    receiveProcess();
+    receiveProcesses();
   }
-  
-  public static void receiveProcess() throws ClassNotFoundException, IOException {
-    ObjectInputStream is = new ObjectInputStream(serverSocket.getInputStream());
-    MigratableProcess ins = (MigratableProcess) is.readObject();
 
-    log("Received " + ins.toString());
-    // when receiving, run single-threaded
-    ins.run();
+  public static void receiveProcesses() throws ClassNotFoundException, IOException {
+    while (true) {
+      ObjectInputStream is = new ObjectInputStream(serverSocket.getInputStream());
+      MigratableProcess ins = (MigratableProcess) is.readObject();
+
+      log("Received " + ins.toString());
+      // when receiving, run single-threaded
+      ins.run();
+    }
   }
 }
