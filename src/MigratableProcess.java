5ebcd2f45095e708a0e9fc420d2a6cc47ec2f59e
‘michaelzhhan1 <hanz@cs.cmu.edu>
2014-09-11 20:02:52 -0400

    migratable

--- src/MigratableProcess.java
+++ src/MigratableProcess.java
@@ -1,69 +1,78 @@
 import java.io.*;
 
+/***
+ * This class acts as the base class for other migratable process
+ * its main function is to provide a thread suspend mechanism 
+ *    
+ * 
+ *
+ */
+
 public abstract class MigratableProcess implements Serializable, Runnable {
-  public static boolean pauseEveryLoop = true;
+  public static boolean pauseEveryLoop = true;   //sleep flag to make the process slow enough for migrate
 
-  public int id;
+  public int id;  //process id, given to Server to tell which process it is
 
-  public boolean debug = true;
+  public boolean debug = true;   //for debug only
   
-  public boolean finished = false;
+  public boolean finished = false; //flag for detect whether the task has been finished yet
 
-  private transient volatile boolean suspending = false;
+  private transient volatile boolean suspending = false;   // flag for process sleeping 
 
-  public void log(String message) {
+  public void log(String message) {  //for debug
     if (debug) {
       System.out.println("[" + toString() + "] " + message);
     }
   }
   
-  public void error(String message) {
+  public void error(String message) { //for error message
     System.err.println("[" + toString() + "] " + message);
   }
 
   // Return false to exit the thread
+  // inherited by other migratable process to do actual work
   public abstract boolean continueRunning() throws Exception;
 
+  //for thread running
   public void run() {
     log("RUNNING");
 
     try {
-      while (!suspending && !finished) {
-        finished = !continueRunning();
-        if (pauseEveryLoop) {
+      while (!suspending && !finished) {  //not finished yet and not suspending, which means running!
+        finished = !continueRunning();   //finished or not, returned by continueRunning function
+        if (pauseEveryLoop) {   //just make it slow!
           //log("Sleeping (still running)");
           Thread.sleep(1000);
         }
       }
-    } catch (Exception e) {
+    } catch (Exception e) { //debug message
+      
       error(e.getMessage());
       e.printStackTrace();
     }
 
-    if (suspending) {
+    if (suspending) {  //
       log("EXITING (suspended)");
     } else {
       log("EXITING (finished)");
-      // notify server or something
-      // ProcessManager.removeProcess(this);
     }
 
     suspending = false;
   }
 
-  public void suspend() {
+  public void suspend() { 
     log("SUSPENDING");
     suspending = true;
-    while (!finished && suspending)
-      ;
+    while (!finished && suspending) //make sure it has not finished, if finished, suspending makes no sense
+     ;
   }
   
-  public boolean isFinished() {
+  public boolean isFinished() {  //return finished
     return finished;
   }
 
   @Override
-  public String toString() {
+  public String toString() {  //give process id, inherited by other migratable process
     return this.getClass().getName() + "#" + id;
   }
 }
